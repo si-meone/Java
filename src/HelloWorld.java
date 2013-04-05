7fa82608de0134daa5ecf277ae64df7289454654
Nasrallah <simon_nasrallah@hotmail.com>
2013-04-05 18:52:08 +0100

    src files

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class HelloWorld {
+    public HelloWorld() {
+        JFrame frame = new JFrame("HelloWorld");
+        frame.setSize(new Dimension(300, 300));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new HelloWorld();
+    }
+}

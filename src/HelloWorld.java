a0fd766d975c78399be213949d297c3d1aa9646d
Nasrallah <simon_nasrallah@hotmail.com>
2013-04-05 21:19:07 +0100

    rejar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
-import javax.swing.JFrame;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
 public class HelloWorld {
     public HelloWorld() {
         JFrame frame = new JFrame("HelloWorld7");
+
+        JLabel label = new JLabel("Rachel");
+        label.setFont(new Font("Serif", Font.PLAIN, 36));
+        frame.add(label);
         frame.setSize(new Dimension(300, 300));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);

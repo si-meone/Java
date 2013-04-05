2c6dd553efb525d86ecc84b85ec5a07b819f05bb
Nasrallah <simon_nasrallah@hotmail.com>
2013-04-05 21:30:43 +0100

    rejar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
 
 public class HelloWorld {
     public HelloWorld() {
         JFrame frame = new JFrame("HelloWorld7");
 
-        JLabel label = new JLabel("Rachel");
+        JLabel label = new JLabel(String.valueOf(new Date()));
         label.setFont(new Font("Serif", Font.PLAIN, 36));
         frame.add(label);
         frame.setSize(new Dimension(300, 300));
